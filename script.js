911d1210223ae32da6bb914733f707d8715ba851
Bryce Pingul <brycepingul@gmail.com>
2020-06-11 18:27:46 -0700

    running script, generates password

---
+++ script.js
@@ -0,0 +1,63 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var lettersLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var lettersUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  var specialChar = ["`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "{", "]", "}", ";", ":", "'", "/", "?", ".", ">", ",", "<"];
+  
+  var pWordChar = []
+
+ var length = prompt("Number of Characters?");
+    
+    var lowerCase = confirm("Lower Case Letters?")
+  if (lowerCase === true){
+    pWordChar = pWordChar.concat(lettersLowerCase)
+  }
+
+    var UpperCase = confirm("Upper Case Letters?")
+  if (UpperCase === true) {
+    pWordChar = pWordChar.concat(lettersUpperCase)
+  }
+
+    var numChar = confirm("Numbers?")
+  if (numChar===true) {
+    pWordChar = pWordChar.concat(num)
+  }
+
+      var spChar = confirm("Special Characters?")
+  if (spChar===true) {
+      pWordChar = pWordChar.concat(specialChar)
+  }
+
+      console.log(pWordChar)
+
+        // create for loop
+    var testPWORD = "" 
+  for (i=0; i<length; i++) {
+    char= pWordChar[Math.floor(Math.random()*pWordChar.length)]
+    testPWORD += char
+  }
+
+  return testPWORD
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+
+
+
+  passwordText.value = password;
+  
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
