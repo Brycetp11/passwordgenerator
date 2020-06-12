a90441e5f9aa27bc3845e3ebc1464ed3f4d88ba1
Bryce Pingul <brycepingul@gmail.com>
2020-06-12 09:43:15 -0700

    adding while loop for number of characters

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  
   var lettersLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
   var lettersUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -12,36 +13,43 @@ function generatePassword() {
   
   var pWordChar = []
 
- var length = prompt("Number of Characters?");
+  var length = prompt("Number of Characters? Between 8 and 128 characters.");
+    while (length<=8 || length>=128) {
+      length = prompt("Number of Characters? Between 8 and 128 characters.")
+    }
     
+
     var lowerCase = confirm("Lower Case Letters?")
   if (lowerCase === true){
     pWordChar = pWordChar.concat(lettersLowerCase)
   }
+
+
 
     var UpperCase = confirm("Upper Case Letters?")
   if (UpperCase === true) {
     pWordChar = pWordChar.concat(lettersUpperCase)
   }
 
+
     var numChar = confirm("Numbers?")
   if (numChar===true) {
     pWordChar = pWordChar.concat(num)
   }
+
 
       var spChar = confirm("Special Characters?")
   if (spChar===true) {
       pWordChar = pWordChar.concat(specialChar)
   }
 
-      console.log(pWordChar)
 
-        // create for loop
     var testPWORD = "" 
   for (i=0; i<length; i++) {
     char= pWordChar[Math.floor(Math.random()*pWordChar.length)]
     testPWORD += char
   }
+
 
   return testPWORD
 }
